dce85bdccd72e094137daaf96913c0243bc34a7e
wibus-wee <1596355173@qq.com>
2023-07-10 21:05:54 +0800

    docs: shiro theme!

---
+++ components/Video/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef } from 'react';
+
+export default function Video({ src }) {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null, 
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          videoRef.current.play();
+        } else {
+          videoRef.current.pause();
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+    observer.observe(videoRef.current);
+
+    return () => {
+      observer.unobserve(videoRef.current);
+    };
+  }, [src]);
+
+  return (
+    <video
+      ref={videoRef}
+      muted
+      autoPlay
+      playsInline
+      loop
+      controls
+      className="mt-6 rounded-xl border dark:border-zinc-800"
+    >
+      <source src={src} type="video/mp4" />
+    </video>
+  );
+}
