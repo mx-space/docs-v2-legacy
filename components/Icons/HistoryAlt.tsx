2bb41085f4b0309e74ee9bde39e12cb6a2897b20
浮心物语 <108316419+wuhang2003@users.noreply.github.com>
2023-08-06 22:01:35 +0800

    docs: fix backup icon
    Signed-off-by: 浮心物语 <108316419+wuhang2003@users.noreply.github.com>

--- components/Icons/HistoryAlt.tsx
+++ components/Icons/HistoryAlt.tsx
@@ -1,7 +1,7 @@
 import { SVGProps } from "react";
 
-export function UilHistoryAlt(props: SVGProps<SVGSVGElement>) {
+export function UimHistoryAlt(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.44,2A10,10,0,0,0,4.56,4.77V3a1,1,0,0,0-2,0V7.5a1,1,0,0,0,1,1H8.06a1,1,0,0,0,0-2H5.66A8,8,0,1,1,11.44,20a1,1,0,1,0,0,2,10,10,0,1,0,0-20Zm0,6a1,1,0,0,0-1,1v3a1,1,0,0,0,1,1h2a1,1,0,0,0,0-2h-1V9A1,1,0,0,0,11.44,8Z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M12 2a10.017 10.017 0 0 0-7 2.877V3a1 1 0 0 0-2 0v4.5a1 1 0 0 0 1 1h4.5a1 1 0 0 0 0-2H6.218A7.992 7.992 0 1 1 12 20a1 1 0 0 0 0 2a10 10 0 0 0 0-20Z"></path><path fill="currentColor" d="M14 13h-2a1 1 0 0 1-1-1V9a1 1 0 0 1 2 0v2h1a1 1 0 0 1 0 2Z"></path><path fill="currentColor" d="M12 4a8.008 8.008 0 0 0-5.782 2.5H8.5a1 1 0 0 1 0 2H4a.989.989 0 0 1-.976-.88A9.977 9.977 0 0 0 12 22a1 1 0 0 1 0-2a8 8 0 0 0 0-16Zm2 9h-2a1 1 0 0 1-1-1V9a1 1 0 0 1 2 0v2h1a1 1 0 0 1 0 2Z" opacity=".5"></path></svg>
   )
 }
