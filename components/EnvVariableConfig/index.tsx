d8a5b3f9dc680c6dcca5e18d199a3ef0653713bf
wibus-wee <1596355173@qq.com>
2023-09-08 23:15:32 +0800

    perf(components): dark mode support & friendly notices

--- components/EnvVariableConfig/index.tsx
+++ components/EnvVariableConfig/index.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-import copy from 'copy-to-clipboard';
+import { useState } from "react";
+import copy from "copy-to-clipboard";
+import clsx from "clsx";
 
 export function EnvVariableConfig({ variableNames }) {
-  const [values, setValues] = useState(Array(variableNames.length).fill(''));
+  const [values, setValues] = useState(Array(variableNames.length).fill(""));
 
   const handleCopy = () => {
-    const envContent = variableNames.map((name, index) => `${name.key}=${values[index]}`).join('\n');
+    const envContent = variableNames
+      .map((name, index) => `${name.key}=${values[index]}`)
+      .join("\n");
     copy(envContent);
   };
 
@@ -21,20 +24,23 @@ export function EnvVariableConfig({ variableNames }) {
         <div key={`${name.key}`} className="flex items-center space-x-4 mb-4">
           <input
             type="text"
-            className="border rounded px-2 py-2 w-1/2 bg-transparent focus:outline-none focus:border-black hover:border-white-400 transition duration-300 font-[400] font-sans text-sm cursor-not-allowed"
+            className={clsx(
+              "border rounded px-2 py-2 w-1/2 bg-transparent focus:outline-none focus:border-black hover:border-white-400 transition duration-300 font-[400] font-sans text-sm cursor-not-allowed",
+              "dark:text-white dark:border-gray-700 dark:hover:border-gray-400 dark:bg-gray-800"
+            )}
             value={name.name || name.key}
             data-tip={name.key}
             onMouseOver={(e: any) => {
-              e.target.style.color = 'transparent';
+              e.target.style.color = "transparent";
               setTimeout(() => {
-                e.target.style.color = 'inherit';
+                e.target.style.color = "inherit";
                 e.target.value = e.target.dataset.tip;
               }, 300);
             }}
             onMouseLeave={(e: any) => {
-              e.target.style.color = 'transparent';
+              e.target.style.color = "transparent";
               setTimeout(() => {
-                e.target.style.color = 'inherit';
+                e.target.style.color = "inherit";
                 e.target.value = name.name || name.key;
               }, 300);
             }}
@@ -43,7 +49,11 @@ export function EnvVariableConfig({ variableNames }) {
           <input
             type="text"
             // :focus-visible outline none
-            className="border rounded px-2 py-2 w-1/2 focus:outline-none focus:border-black hover:border-gray-400 transition duration-300 font-[400] font-sans text-sm" style={{ outline: "none", boxShadow: "none" }}
+            className={clsx(
+              "border rounded px-2 py-2 w-1/2 focus:outline-none focus:border-black hover:border-gray-400 transition duration-300 font-[400] font-sans text-sm",
+              "dark:text-white dark:border-gray-700 dark:hover:border-gray-400 dark:bg-gray-800"
+            )}
+            style={{ outline: "none", boxShadow: "none" }}
             placeholder={`Enter value...`}
             value={values[index]}
             onChange={(e) => handleChange(index, e.target.value)}
@@ -54,7 +64,10 @@ export function EnvVariableConfig({ variableNames }) {
         <h2 className="sr-only">环境变量配置</h2>
         <button
           type="button"
-          className="border bg-black w-full text-white px-4 py-2 rounded-lg text-sm transform transition-all duration-300 focus:outline-none hover:bg-gray-700"
+          className={clsx(
+            "border bg-black w-full text-white px-4 py-2 rounded-lg text-sm transform transition-all duration-300 focus:outline-none hover:bg-gray-700",
+            "dark:bg-gray-800 dark:border-gray-500"
+          )}
           onClick={handleCopy}
         >
           复制
