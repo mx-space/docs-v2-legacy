311e0d169cc1d7f7031bd4b09596c05418d150b8
Innei <i@innei.in>
2024-03-29 21:55:31 +0800

    feat: add tracker
    Signed-off-by: Innei <i@innei.in>

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../public/style.css'
+import { OpenpanelProvider } from '@openpanel/nextjs'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <OpenpanelProvider
+        url="https://api.openpanel.dev"
+        clientId="feee95af-b002-4b49-a90f-2d6b9c7b7e17"
+        trackScreenViews={true}
+        trackOutgoingLinks={true}
+      />
+      <Component {...pageProps} />
+    </>
+  )
+}
