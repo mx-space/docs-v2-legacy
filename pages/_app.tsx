3dd7a1e7653c977e93be5c20adc6108b455a8638
Innei <i@innei.in>
2024-04-07 11:18:05 +0800

    chore: add clarity
    Signed-off-by: Innei <i@innei.in>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+import Script from 'next/script'
 import '../public/style.css'
 import { OpenpanelProvider } from '@openpanel/nextjs'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +12,13 @@ export default function App({ Component, pageProps }) {
         trackScreenViews={true}
         trackOutgoingLinks={true}
       />
+      <Head>
+        <Script>{`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "lsoepndf05");`}</Script>
+      </Head>
       <Component {...pageProps} />
     </>
   )
