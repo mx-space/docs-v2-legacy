abbb94a5536af320fa1940cf34f6495825f27cae
wuhang2003 <108316419+wuhang2003@users.noreply.github.com>
2024-09-08 10:07:27 +0000

    feat: add oauth

---
+++ components/Icons/UilKeySkeletonAlt.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from 'react'
+
+export function UilKeySkeletonAlt(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m21.71 6.53l-1.42-1.41l1.42-1.41a1 1 0 1 0-1.42-1.42L9.75 12.83a5 5 0 1 0 1.42 1.42l4.88-4.89l1.41 1.42a1 1 0 0 0 .71.29a1 1 0 0 0 .71-.29a1 1 0 0 0 0-1.42L17.46 8l1.42-1.42L20.29 8a1 1 0 0 0 .71.29a1 1 0 0 0 .71-.29a1 1 0 0 0 0-1.47M7 20a3 3 0 1 1 3-3a3 3 0 0 1-3 3"></path></svg>
+  )
+}
